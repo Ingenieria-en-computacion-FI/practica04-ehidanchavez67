41f615256a5e5f97b96575cb26ae7a2c597f7789
ehidanchavez67 <ehidanchavez@unam.edu>
2026-03-12 18:25:04 -0600

    Update nodo.h

--- exercies/nodo.h
+++ exercies/nodo.h
@@ -2,32 +2,23 @@
 #define NODO_H
 
 #include <stdbool.h>
-
-/* Estructura Nodo */
-typedef struct Nodo {
+struct nodo{
     int dato;
-    struct Nodo* siguiente;
-} Nodo;
+    struct nodo* siguiente;
+};
+typedef struct nodo Nodo;
 
-/* Ejercicio 1: Crear un nodo dinámicamente */
 Nodo* crearNodo(int valor);
-
-/* Ejercicio 2: Crear varios nodos con calloc */
 Nodo* crearArregloNodos(int n);
-
-/* Ejercicio 3: Agregar un nodo usando realloc */
 Nodo* agregarNodo(Nodo* arreglo, int* n, int valor);
-
-/* Ejercicio 4: Liberar memoria de nodos */
 void liberarNodos(Nodo* arreglo, int n);
-
-/* Ejercicio 5: Construir nodos enlazados manualmente */
 Nodo* construirTresNodos();
-
-/* Ejercicio 6: Contar nodos */
 int contarNodos(Nodo* inicio);
-
-/* Ejercicio 7: Crear nodos leyendo números */
 Nodo* crearNodosPorEntrada();
 
 #endif
+
+
+
+
+
